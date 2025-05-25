0bf8efe3dd2762b0308167422ed240d542d6be1c
Andrei Stanovykh <astanovykh@gmail.com>
2025-05-26 01:21:40 +0500

    проверка тг 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter.jsx';
 import { useTheme } from './hooks/useTheme.js';
@@ -6,9 +7,16 @@ import { ThemeContext } from './contexts/ThemeContext.js';
 
 function App() {
 	const { theme, setTheme } = useTheme();
-	const tg = window.Telegram.WebApp;
-	const user = tg.initDataUnsafe.user;
-	console.log(user);
+	useEffect(() => {
+		if (window.Telegram && window.Telegram.WebApp) {
+			const tg = window.Telegram.WebApp;
+			tg.ready();
+			const user = tg.initDataUnsafe.user;
+			console.log(user);
+		} else {
+			console.log('Запущено вне Telegram WebApp');
+		}
+	}, []);
 
 	return (
 		<ThemeContext.Provider value={{ theme, setTheme }}>
